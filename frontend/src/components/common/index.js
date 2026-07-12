8e2de6f77346d0824df572fff08054d962859aee
bpereiracav-sys <bpereiracav@gmail.com>
2026-07-12 15:10:08 +0100

    Export components from common index file

---
+++ frontend/src/components/common/index.js
@@ -0,0 +1,4 @@
+export { default as PageHero } from './PageHero';
+export { default as StatusBadge } from './StatusBadge';
+export { default as CardActionMenu } from './CardActionMenu';
+export { default as CompactAccessCard } from './CompactAccessCard';
