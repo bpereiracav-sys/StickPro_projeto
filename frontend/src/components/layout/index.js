4dbf91b63aed0fb067e2cd511c385bb06bc92c63
bpereiracav-sys <bpereiracav@gmail.com>
2026-07-12 10:56:49 +0100

    Export layout components from index.js

---
+++ frontend/src/components/layout/index.js
@@ -0,0 +1,4 @@
+export { default as PageShell } from './PageShell';
+export { default as PageSection } from './PageSection';
+export { default as PageActions } from './PageActions';
+export { default as PageMetrics } from './PageMetrics';
