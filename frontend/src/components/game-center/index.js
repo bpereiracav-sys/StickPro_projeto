6bf9dd7282ffc30a15e115e52d0d6d502b39b38b
bpereiracav-sys <bpereiracav@gmail.com>
2026-07-13 16:33:45 +0100

    Create index.js for game center component exports

---
+++ frontend/src/components/game-center/index.js
@@ -0,0 +1,5 @@
+export { default as GameHeader } from './GameHeader';
+export { default as GameTabs } from './GameTabs';
+export { default as MatchLineup } from './MatchLineup';
+export { default as TechnicalAssistantCard } from './TechnicalAssistantCard';
+export { default as MatchCenterPlaceholder } from './MatchCenterPlaceholder';
