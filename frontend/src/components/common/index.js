83f56d4966fa482c0e2467229af0f42c80e503dc
bpereiracav-sys <bpereiracav@gmail.com>
2026-07-13 17:08:14 +0100

    Add MatchPremiumHero and MatchOverviewDashboard exports

--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -2,3 +2,5 @@ export { default as PageHero } from './PageHero';
 export { default as StatusBadge } from './StatusBadge';
 export { default as CardActionMenu } from './CardActionMenu';
 export { default as CompactAccessCard } from './CompactAccessCard';
+export { default as MatchPremiumHero } from './MatchPremiumHero';
+export { default as MatchOverviewDashboard } from './MatchOverviewDashboard';
